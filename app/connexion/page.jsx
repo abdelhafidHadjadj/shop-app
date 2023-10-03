536f38fecd76458076d5a75ed508ba4c400b5d7b
Abdelhafid HADJADJ <98639038+abdelhafidHadjadj@users.noreply.github.com>
2023-10-03 23:38:13 +0100

    add forms

--- app/connexion/page.jsx
+++ app/connexion/page.jsx
@@ -1,10 +1,64 @@
+"use client";
+import CustomButton from "@/components/CustomButton";
 import FormField from "@/components/FormField";
-import React from "react";
+import React, { useState } from "react";
 
 const Connexion = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("====================================");
+    console.log(firstName);
+    console.log("submitted");
+    console.log("====================================");
+  };
+  console.log("====================================");
+  console.log(firstName);
+  console.log("====================================");
   return (
     <div>
-      <form></form>
+      <form onSubmit={handleSubmit}>
+        <FormField
+          label={"First name"}
+          isTextArea={false}
+          state={firstName}
+          inputName={"firstName"}
+          setState={setFirstName}
+          inputType={"text"}
+        />
+        <FormField
+          label={"Last name"}
+          isTextArea={false}
+          inputName={"lastName"}
+          setState={setLastName}
+          state={firstName}
+          inputType={"text"}
+        />
+        <FormField
+          label={"Email"}
+          isTextArea={false}
+          inputName={"email"}
+          setState={setEmail}
+          state={firstName}
+          inputType={"email"}
+        />
+        <FormField
+          label={"Password"}
+          isTextArea={false}
+          inputName={"password"}
+          setState={setPassword}
+          inputType={"password"}
+        />
+        <CustomButton
+          btnType={"submit"}
+          title={"Sign Up"}
+          disabled={false}
+          containerStyle={"primary-btn"}
+        />
+      </form>
     </div>
   );
 };
